c368139bbd6ef81820a97ed0288f0d1bec786574
Alton Bell Smythe <abellsmythe@gmail.com>
2024-01-25 12:03:22 -0300

    Add server graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { initApp } from './app';
 import { initServer } from './server';
+import { terminate } from './terminate';
 
 async function boot() {
   const app = initApp();
@@ -9,6 +10,17 @@ async function boot() {
     // tslint:disable-next-line no-console
     console.log(`Server listening on port 3000`);
   });
+
+  // Graceful Shutdown
+  const shutdown = terminate(server, { coredump: false, timeout: 500 });
+
+  process.on('unhandledRejection', shutdown(1, 'Unhandled Promise'));
+  process.on('uncaughtException', shutdown(1, 'Unexpected Error'));
+  process.on('SIGTERM', shutdown(0, 'SIGTERM'));
+  process.on('SIGINT', shutdown(0, 'SIGINT'));
 }
 
-boot().catch(console.error);
+boot().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
