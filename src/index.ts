8c7d50b6159416d5cf7a2b5a9ea3917f6450b6cf
Alton Bell Smythe <abellsmythe@gmail.com>
2024-01-25 11:58:22 -0300

    Update entry point with app and server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('HELLO!! :wave:');
+import { initApp } from './app';
+import { initServer } from './server';
+
+async function boot() {
+  const app = initApp();
+  const server = initServer(app);
+
+  app.listen(3000, '0.0.0.0', () => {
+    // tslint:disable-next-line no-console
+    console.log(`Server listening on port 3000`);
+  });
+}
+
+boot().catch(console.error);
