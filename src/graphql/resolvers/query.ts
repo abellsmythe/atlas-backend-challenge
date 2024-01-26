d880dcf37f6d02718a2c708482167f31680cfdc2
Alton Bell Smythe <abellsmythe@gmail.com>
2024-01-26 12:56:20 -0300

    Add graphql query resolver

---
+++ src/graphql/resolvers/query.ts
@@ -0,0 +1,9 @@
+// Couldn't find a proper typing for the Query resolvers
+//  - parent
+//  - args
+//  - contextValue
+//  - info
+export const queryResolver = {
+  plans: (_parent: unknown, _args: unknown) => [],
+  subscriptions: (_parent: unknown, _args: unknown) => [],
+};
