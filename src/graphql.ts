3d9000cc4268d46b0414748716a536df804cc1be
Alton Bell Smythe <abellsmythe@gmail.com>
2024-01-26 20:45:28 -0300

    Add graphql context

--- src/graphql.ts
+++ src/graphql.ts
@@ -2,11 +2,13 @@ import { ApolloServer } from 'apollo-server-express';
 
 import type { ServerRegistration } from 'apollo-server-express';
 
+import { context } from './graphql/context';
 import { typeDefs } from './graphql/schema';
 import { resolvers } from './graphql/resolvers';
 
 export async function initGraphQLServer(app: ServerRegistration['app']) {
   const server = new ApolloServer({
+    context,
     resolvers,
     typeDefs,
   });
