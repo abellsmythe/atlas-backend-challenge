95a7a0cd143550b24d1b4346fc91d20e06874f71
Alton Bell Smythe <abellsmythe@gmail.com>
2024-01-26 14:06:08 -0300

    Update graphql schema

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -8,16 +8,147 @@ export const typeDefs = gql`
     subscriptions(customerId: Int): [Subscription]
   }
 
+  enum Currency {
+    usd
+  }
+  
+  enum BillingFrequency {
+    annual
+    monthly
+  }
+
+  enum SupportLevel {
+    basic
+    dedicated
+    priority
+  }
+
+  enum Analytics {
+    advanced
+    basic
+    none
+  }
+
+  enum Integrations {
+    custom
+    none
+  }
+
+  enum CountryCode {
+    arg
+    usa
+  }
+
+  enum SubscriptionStatus {
+    active
+    canceled
+    paused
+    trial
+  }
+
+  enum InvoiceStatus {
+    draft
+    open
+    paid
+    uncollectible
+    voided
+  }
+  
+  enum Role {
+    admin
+    su
+    user
+  }
+  
+  enum PaymentStatus {
+    approved
+    pending
+    processing
+    rejected
+  }
+  
+  enum PaymentMethod {
+    bankTransfer
+    credit
+    debit
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    firstName: String!
+    lastName: String!
+    password: String!
+    avatar: String
+    role: Role!
+    createdAt: Float!
+    updatedAt: Float!
+    deletedAt: Float
+  }
+
+  type Customer {
+    id: ID!
+    userId: User!
+    createdAt: Float!
+    updatedAt: Float!
+    deletedAt: Float
+  }
+
   type Plan {
     id: ID!
     name: String!
     description: String
+    price: Float!
+    currency: Currency!
+    billingFrequency: BillingFrequency!
+    storage: Int!
+    supportLevel: SupportLevel!
+    analytics: Analytics!
+    integrations: Integrations!
+    annualDiscount: Int!
+    createdAt: Float!
+    updatedAt: Float!
+    deletedAt: Float
   }
 
   type Subscription {
     id: ID!
+    customer: Customer!
     plan: Plan!
     startsAt: Float!
     endsAt: Float
+    createdAt: Float!
+    updatedAt: Float!
+    deletedAt: Float
+  }
+  
+  type Invoice {
+    id: ID!
+    customer: Customer!
+    subscription: Subscription!
+    status: InvoiceStatus!
+    amount: Float!
+    currency: Currency!
+    dueDateAt: Float!
+    billingAddress: String!
+    billingZipCode: String!
+    billingCountryCode: CountryCode!
+    billingFullName: String!
+    createdAt: Float!
+    updatedAt: Float!
+    deletedAt: Float
+  }
+  
+  type Payment {
+    id: ID!
+    customer: Customer!
+    invoice: Invoice!
+    status: PaymentStatus!
+    amount: Float!
+    currency: Currency!
+    method: PaymentMethod!
+    createdAt: Float!
+    updatedAt: Float!
+    deletedAt: Float
   }
 `;
