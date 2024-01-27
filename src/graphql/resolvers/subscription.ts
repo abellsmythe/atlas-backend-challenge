3c20fd1121ee43db18d0431416066caa526e62e0
Alton Bell Smythe <abellsmythe@gmail.com>
2024-01-27 14:34:31 -0300

    Add subscription customer query resolver

--- src/graphql/resolvers/subscription.ts
+++ src/graphql/resolvers/subscription.ts
@@ -4,13 +4,23 @@ import type { Subscription } from '@prisma/client';
 import type { Context } from '../context';
 
 export const subscriptionResolver = {
+  customer: (
+    parent: Subscription,
+    _args: unknown,
+    context: Context,
+    _info: GraphQLResolveInfo
+  ) =>
+    // There must be a way to extract the selected fields instead of query the whole row
+    context.prisma.customer.findFirst({
+      where: { id: parent.customerId, deletedAt: null },
+    }),
   plan: (
     parent: Subscription,
     _args: unknown,
     context: Context,
     _info: GraphQLResolveInfo
   ) =>
-  // There must be a way to extract the selected fields instead of query the whole row
+    // There must be a way to extract the selected fields instead of query the whole row
     context.prisma.plan.findFirst({
       where: { id: parent.planId, deletedAt: null },
     }),
